d9cf55fb7676f1f94f1e667deda24d016a1a3542
Igor Mierzejewski <Igor.Mierzejewski@idemia.com>
2024-10-22 23:54:26 +0200

    working on mobile device alignemnt v6

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,52 @@
 import React from 'react';
+import './styles/Home.css';
 
 function Home() {
     return (
-        <div>
-            <h1>Witaj w naszej aplikacji!</h1>
-            <p>Eksploruj świat z nowym stylem i komfortem.</p>
+        <div className="home">
+            <table className="opening-hours">
+                <thead>
+                <tr>
+                    <th>Dzień tygodnia</th>
+                    <th>Godziny otwarcia</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Poniedziałek</td>
+                    <td>9:30 - 18:00</td>
+                </tr>
+                <tr>
+                    <td>Wtorek</td>
+                    <td>9:30 - 18:00</td>
+                </tr>
+                <tr>
+                    <td>Środa</td>
+                    <td>9:30 - 18:00</td>
+                </tr>
+                <tr>
+                    <td>Czwartek</td>
+                    <td>9:30 - 18:00</td>
+                </tr>
+                <tr>
+                    <td>Piątek</td>
+                    <td>9:30 - 18:00</td>
+                </tr>
+                <tr>
+                    <td>Sobota</td>
+                    <td>9:30 - 18:00</td>
+                </tr>
+                <tr>
+                    <td>Niedziela</td>
+                    <td>9:30 - 16:00</td>
+                </tr>
+                </tbody>
+            </table>
+            <p className="note">
+                Kwiaciarnia, która nieprzerwanie działa przy ulicy Tumskiej w Płocku, to prawdziwa pasja właścicielki,
+                która ma już ponad 75 lat. Dla niej kwiaciarnia to całe życie, a obok niej obecnie pracuje jej córka,
+                kontynuując rodzinną tradycję.
+            </p>
         </div>
     );
 }
